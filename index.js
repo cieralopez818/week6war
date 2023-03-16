26d4cd9c4570aa89cd424598cc3004d5c89bfb2e
Ciera Lopez <123427746+cieralopez818@users.noreply.github.com>
2023-03-15 19:33:59 -0700

    war game

--- index.js
+++ index.js
@@ -5,106 +5,108 @@ class Card {
     this.suit = suit;
     this.face = face;
     this.value = value;
-  }
+    }
 }
-let player1 = {
-    hand:[]
-  };
-  let player2 = {
-    hand:[]
-  };
-
 class Deck {
   constructor() {
     this.deckOfCards = [];
-    // this.entireDeck = this.makeDeck();
-  }
+    this.entireDeck = this.makeDeck();
+}
 
   //make deck function -  begin
-  makeDeck() {
-    let values = [2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K", "A"];
-    let suits = ["Club", "Heart", "Spade", "Diamond"];
-    let deckOfCards = []; //declares an array variable that will hold multiple cards
+    makeDeck() {
+        let values = [2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K", "A"];
+        let suits = ["Club", "Heart", "Spade", "Diamond"];
 
     for (let s = 0; s < suits.length; s++) {
-      console.log("suits:", suits[s]);
+      console.log("suit:", suits[s]);
       for (let x = 0; x < values.length; x++) {
         let card = new Card(suits[s], values[x], x + 2);
         this.deckOfCards.push(card); //pushes an instance to the deckOfCards array
-      }
+        }
     }
-    // deckOfCards = deckOfCards.sort((a, b) => 0.5 - Math.random());
-    console.log(this.deckOfCards);
-    return this.deckOfCards;
-   } //make deck function - end
-  shuffleDeck() {
+};
+    shuffleDeck() {
     for(let i = this.deck - 1; i > 0; i--){
         let j= Math.floor(Math.random() * (i+1));
         let tempIndex = this.deck[i];
         this.deck[i] = this.deck[j];
         this.desk[j] = tempIndex;
         }
-    console.log(this.deck);
+        return shuffleDeck;
     }
-  dealDeck() {
-    console.log(player1.hand);
-    player1.hand=this.deckOfCards.slice(0,26),
-    console.log(player1.hand);
+    deal()  {
+        console.log("deal");
+        let card = this.deck.pop();
+        console.log(card + " has been drawn from the deck.")
+        }
+    }
 
-    console.log(player2.hand);
-    player2.hand=this.deckOfCards.slice(0,26),
-    console.log(player2.hand);
-  }
- } 
-
-const Ciera = new Deck();
-Ciera.makeDeck()
-Ciera.shuffleDeck()
-Ciera.dealDeck()
-
-playGame(Ciera.entireDeck);
-function playGame(deck) {
-  round = 1;
+class players {
+    constructor (name)  {
+        this.name= name;
+        this.point= 0;
+        this.hands= hands
+    }
 }
-let player1Score = 0;
-let player2Score = 0;
-
-console.log("player1:", player1.hand);
-console.log("player2:", player2.hand);
-
-console.log(
-  `Player 1 plays a ${player1.hand[0].face}; Player 2 plays a ${player2.hand[0].face}`
-);
-console.log("card value",player1.hand[8].value);
-(card >= 16)
-// players currently have 26 cards each
-// compare player cards
-//showing if player1 card is > player2 card
-    if('player1  > players2 card'){
-        console.log("give the winning player a point",);
-        // this.scorePlayer1++.push;
-        player1Score++
-        // // this.scorePlayer2++.push;
-        // player2Score++
+class Game  {
+    constructor()   {
+        this.player= [];
     }
+    start() {
+        this.players.push(new Players);
+        this.players.push(new Players);
+        console.log('War', this.players);
 
-//         //showing if player1 card is < player2 card
-        else if("player2 > player1"){
-        console.log("give the winning player a point",);
-        this.scorePlayer2++
-            }
-            else (player1.hand[0].face === player2.hand[0].face);
-            console.log("player1Score => player2Score");
-                player1Score +=1
-                player2Score +=1
-                    alert ("Tie"); //if not > or < then it has to be =
-            // tie
-// //declare which player has the most point or if its a tie
-    console.log(`Player 1 score: ${this.scorePlayer1}`);
-    console.log(`Player 2 score: ${this.scorePlayer2}`);
-            if(this.scorePlayer1 > this.scorePlayer2){
-                console.log("Player 1 Wins");
-            }
-            if(this.scorePlayer2 > this.scorePlayer1){
-                console.log("Player 2 Wins");
-            }
+        let myDeck= new Deck ();
+        myDeck.createDeck();
+
+        let shuffleDeck= myDeck.shuffleDeck();
+        myDeck.dealDeck(this.players, shuffleDeck);
+
+        this.playGame();
+        this.endGame();
+        }
+
+playGame () {
+    console.log('War');
+    let player1 = this.players[0];
+    let player2 = this.players[1];
+
+    let roundWinner = '';
+    let turn = 0;
+
+    while(player1.hands.length!==0 && player2.hands.length!==0){
+        let player1Card= player1.hands.pop();
+        let player2Card= player2.hands.pop();
+
+    if(player1Card.value > player2Card.value){
+        roundWinner= player1.name;
+        player1.points +=1;
+    }
+    else if (player2Card.value > player1Card.value){
+        roundWinner = player2.name;
+        player2.points +=1;        
+    }
+    else {
+        console.log()
+        }
+    }
+}
+endGame() {
+    let gameWinner= '';
+    let player1 = this.players[0];
+    let player2 = this.players[1];
+    let winnerPoints = 0;
+
+    if(player1.points > player2.points){
+        gameWinner = player1.name;
+        winnerPoints = player1.points;
+    } else if (player2.points > player1.points){
+        gameWinner = player2.name;
+        winnerPoints = player2.points;
+    } else {
+        alert("Game Over");
+        }
+    }
+}
